607a7a6d4460bc7cdb3c3a7abae8ea7e5b1fd085
TimChern111 <66830325+TimChern111@users.noreply.github.com>
2020-10-02 14:47:00 +0300

    Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import css from "./css/style.css";
-//posts comments albums photoes todos 
-const name = 'users'
-const url = `https://jsonplaceholder.typicode.com/${name}`
-
-// const xmlReq = new XMLHttpRequest()
+import css from './css/style.css';

27f3a52020535d938170403b6ec9acb7e2a7b9e1
TimChern111 <66830325+TimChern111@users.noreply.github.com>
2020-10-02 10:16:37 +0300

    rec

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import css from "./css/style.css";
+//posts comments albums photoes todos 
+const name = 'users'
+const url = `https://jsonplaceholder.typicode.com/${name}`
+
+const xmlReq = new XMLHttpRequest()
+console.log();
